ff6a491372c67b9d192fb8210bbbe273381bf96c
Aleilton Chavenco <aleilton@gmail.com>
2022-03-23 14:58:50 -0300

    feat: get_min_salary function implement

--- src/insights.py
+++ src/insights.py
@@ -71,20 +71,15 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
+    file_content = read(path)
+    salaries = set()
+    for line in file_content:
+        if line["min_salary"] != "":
+            try:
+                salaries.add(float(line["min_salary"]))
+            except ValueError:
+                pass
+    return sorted(salaries)[0]
     pass
 
 
